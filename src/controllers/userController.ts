df1b5f014f7d56d9204cfb7a1d5159aff64880b0
Shahid <97959691+shanialy@users.noreply.github.com>
2025-12-29 22:00:54 +0500

    fix: pupeeter fixes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -250,7 +250,17 @@ async function generateStyledPDFAndUpload(data: any) {
   `;
 
   // 2. Launch Puppeteer
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+    "--no-zygote",
+    "--single-process"
+  ]
+});
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
 
